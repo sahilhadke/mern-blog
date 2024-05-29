b7b65e61ab00062f3dbe9f62c4b881fd35b1bd0c
Sahil Hadke <sahil.hadke@gmail.com>
2024-05-29 07:49:06 -0700

    configured mongodb

--- api/index.js
+++ api/index.js
@@ -1,11 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
 const app = express()
+dotenv.config()
+const port = process.env.PORT || 3001
 
-app.listen(3001, () => {
-  console.log('Server is running on port 3001')
+app.listen(port, () => {
+  console.log('Server is running on port ' + port)
 })
 
 app.get('/', (req, res) => {
+    mongoose.connect(process.env.MONGO).then( () => {
+      console.log('Connected to MongoDB')
+    }).catch( (err) => {
+      console.log(err)
+    })
     res.send('Hello World!!!')
 })
