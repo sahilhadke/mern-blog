6aec01169f9553995315002a7082992b810bd218
Sahil Hadke <sahil.hadke@gmail.com>
2024-06-04 09:28:18 -0700

    signup api

--- api/index.js
+++ api/index.js
@@ -1,20 +1,27 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
-const app = express()
+// configure dotenv to use .env file
 dotenv.config()
 const port = process.env.PORT || 3001
 
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log('Connected to MongoDB')
+});
+
+const app = express()
+
+// middleware
+app.use(express.json())
+app.get('/', (req, res) => {
+  res.send('Hello World!!!')
+})
+app.use('/api/users', userRoutes)
+app.use('/api/auth', authRoutes)
+
 app.listen(port, () => {
   console.log('Server is running on port ' + port)
-})
-
-app.get('/', (req, res) => {
-    mongoose.connect(process.env.MONGO).then( () => {
-      console.log('Connected to MongoDB')
-    }).catch( (err) => {
-      console.log(err)
-    })
-    res.send('Hello World!!!')
 })
