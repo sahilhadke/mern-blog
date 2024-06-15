5b5c426c4f63eb1c6059ac1466896c07137ec161
Sahil Hadke <sahil.hadke@gmail.com>
2024-06-15 13:20:33 -0700

    dashboard UI

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import DashProfile from '../components/DashProfile'
+import DashSidebar from '../components/DashSidebar'
+
 export default function App() {
+
+    const [tab, setTab] = useState('')
+
+    const location = useLocation()
+    
+    useEffect(() => {
+      const urlParams = new URLSearchParams(location.search)
+      const tabFormUrl = urlParams.get('tab')
+      setTab(tabFormUrl)
+    }, [location.search])
+
     return (
-      <div className="text-3xl text-red-500">Dashboard</div>
+      <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      {/* profile... */}
+      {tab === 'profile' && <DashProfile />}
+      
+    </div>
     )
   }
   
