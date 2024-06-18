c35d227a49450be558929c2b520769edb78d53fa
Sahil Hadke <sahil.hadke@gmail.com>
2024-06-17 22:10:45 -0700

    upload image functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,16 +1,122 @@
-import {useSelector} from 'react-redux'
-import {Button, TextInput} from 'flowbite-react'
+/* eslint-disable no-unused-vars */
+import {useSelector, useDispatch} from 'react-redux'
+import {useState, useRef, useEffect} from 'react'
+import {Alert, Button, TextInput} from 'flowbite-react'
+import {getStorage, ref, uploadBytesResumable, getDownloadURL} from 'firebase/storage'
+import {app} from '../firebase'
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 export default function DashProfile() {
-    const currentUser = useSelector((state) => state.user.currentUser)
+    const { currentUser, error, loading } = useSelector((state) => state.user);
+    const [imageFile, setImageFile] = useState(null);
+    const [imageFileUrl, setImageFileUrl] = useState(null);
+    const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+    const [imageFileUploadError, setImageFileUploadError] = useState(null);
+    const [imageFileUploading, setImageFileUploading] = useState(false);
+    const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+    const [updateUserError, setUpdateUserError] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+    const [formData, setFormData] = useState({});
+    const filePickerRef = useRef();
+    const dispatch = useDispatch();
+    
+    const handleImageChange = (e) => {
+        const file = (e.target.files[0])
+        if(file){
+            setImageFile(file)
+            setImageFileUrl(URL.createObjectURL(file))
+        }
+    }
+    console.log(imageFileUrl)
+    console.log(imageFile)
+
+    useEffect(() => {
+        if (imageFile) {
+          uploadImage();
+        }
+      }, [imageFile]);
+
+    const uploadImage = async () => {
+        const storage = getStorage(app)
+        const fileName = new Date().getTime() + '_' + imageFile.name
+        const storageRef = ref(storage, fileName)   
+        const uploadTask = uploadBytesResumable(storageRef, imageFile)
+
+
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+
+                setImageFileUploadProgress(progress.toFixed(0))
+
+                console.log('Upload is ' + progress + '% done')
+                switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused')
+                        break
+                    case 'running':
+                        console.log('Upload is running')
+                        break
+                }
+            },
+            (error) => {
+                setImageFileUploadError('Could not upload image')
+                console.log(error)
+                setImageFileUploadProgress(null)
+                setImageFile(null)
+                setImageFileUrl(null)
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    setImageFileUrl(downloadURL)
+
+                    console.log('Upload is complete: ' + downloadURL)
+                    setImageFileUploadProgress(null)
+                })
+                
+
+            }
+        )
+    }
+
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
 
         <h1 className='my-7 text-center font-semibold text-3xl'>profile</h1>
+        {imageFileUploadError && <Alert color='failure'>{imageFileUploadError}</Alert>}
         <form className='flex flex-col gap-4'>
-            
-            <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
+            <input className='hidden' type='file' accept='image/*' onChange={handleImageChange} ref={filePickerRef}/>
+            <div className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full' onClick={()=>{filePickerRef.current.click()}}>
                 
-                <img src={currentUser.photoURL} alt='user' className='rounded-full object-cover h-full w-full border-8 border-[lightgrey ]' />
+                {imageFileUploadProgress && (
+                    <CircularProgressbar 
+                    value={imageFileUploadProgress || 0} 
+                    text={`${imageFileUploadProgress}%`} 
+                    strokeWidth={5}
+                    style={
+                        {
+                            root:{
+                                width: '100%',
+                                height: '100%',
+                                position: 'absolute',
+                                top: 0,
+                                left: 0
+                            },
+                            path:{
+                                stroke: `rgba(62, 152, 199, ${imageFileUploadProgress/100})`
+                            }
+                            
+                        }
+                    }
+                    />
+                )}
+
+                <img 
+                src={imageFileUrl || currentUser.photoURL} 
+                alt='user' 
+                className={`rounded-full object-cover h-full w-full border-8 border-[lightgrey] $ 
+                    {imageFileUploadProgress && imageFileUploadProgress < 100 && 'opacity-60'}`} />
        
             </div>
 
